2f6713fc2845a23e781ef2c10e331671a35a71eb
MurtazaBagwala07 <murtazabag786@gmail.com>
2022-03-19 11:41:45 +0530

    Converted homepage from html css to jsx Added routes Defined a folder structure for project

--- jersea/src/App.js
+++ jersea/src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
+import {Home} from './pages/Home/Home.jsx'
+import { Routes, Route } from 'react-router-dom';
+import Mockman from 'mockman-js'
 
+
+function MockAPI() {
+  return (
+    <div className='MockAPI'>
+      <Mockman />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/products'/>
+      <Route path='/mockman' element={<MockAPI/>} />
+    </Routes>
+    </>
   );
 }
 
